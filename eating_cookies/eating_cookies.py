78227eea06cf7b1abbdcbbb555917070a188050b
spencer-mcguire <smcguire1826@gmail.com>
2020-04-16 11:10:30 -0500

    sort of working

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -12,9 +12,18 @@ def eating_cookies(n, cache=None):
     # __base_case__
     # if n = 1 return 1
     # if n = 0 return 0
-    pass
+
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        # I can either either eat 1 - 2 - 3 cookies at a time
+        return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+    # print(n)
 
 
+print(eating_cookies(10))
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         num_cookies = int(sys.argv[1])
