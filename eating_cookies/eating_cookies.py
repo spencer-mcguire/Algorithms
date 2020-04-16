d0abd1cf0cb1e66b01f8be83a3ea9f1f46667d5b
spencer-mcguire <smcguire1826@gmail.com>
2020-04-16 11:50:56 -0500

    class fix

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -12,15 +12,26 @@ def eating_cookies(n, cache=None):
     # __base_case__
     # if n = 1 return 1
     # if n = 0 return 0
+    if cache is None:
+        cache = [0] * (n + 1)
+    if n <= 1:
+        cache[n] = 1
+    elif n == 2:
+        cache[n] = 2
+    elif cache[n] == 0:
+        cache[n] = eating_cookies(
+            n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    return cache[n]
 
-    if n < 0:
+
+"""    if n < 0:
         return 0
     elif n == 0:
         return 1
     else:
         # I can either either eat 1 - 2 - 3 cookies at a time
-        return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
-    # print(n)
+        return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)"""
+# print(n)
 
 
 print(eating_cookies(10))
